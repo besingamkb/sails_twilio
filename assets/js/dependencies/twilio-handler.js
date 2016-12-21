c3802ed727d3b8f3e5c02c34665a5dc35267a7b8
Mark Kevin Besinga <besingamkb@gmail.com>
2016-12-21 22:33:55 +0800

    webrtc

--- assets/js/dependencies/twilio-handler.js
+++ assets/js/dependencies/twilio-handler.js
@@ -10,22 +10,58 @@ Twilio.Device.setup(setup_token);
 // call:
 Twilio.Device.incoming(function(connection) {
     //For demo purposed, automatically accept the call
-    connection.accept();
-    $('p#status').html('Call in progress...');
+    // connection.accept();
+    console.log(connection);
+
+    window.connection = connection;
+
+    $('span.from').text(connection.parameters.From);
+    $('div#incoming_div').show();
+    // $('p#status').html('Call in progress...');
 });
 
 // Register an event handler for when a call ends for any reason
 Twilio.Device.disconnect(function(connection) {
-    $('p#status').html('Awaiting incoming call...');
+    $('#outbound_call').show();
+});
+
+// Diconnected
+Twilio.Device.disconnect(function (connection) {
+    $('div#incoming_div').hide();
+});
+
+// cancel
+Twilio.Device.cancel(function (connection) {
+    $('div#incoming_div').hide();
 });
 
 // Add a click event for the button, which will hang up the current
 // call when clicked:
 $('#hangup').click(function() {
     Twilio.Device.disconnectAll();
+
+    $('#status').text('Call Ended');
+
+    setTimeout(function() {
+        $('#status').text('');
+    }, 1000);
 });
 
-$('#call').click(function(e) {
+$('#outbound_call').click(function(e) {
+    var params = {
+        "To": $('#number').val()
+    };
 
+    $('#outbound_call').hide();
+
+    $('#status').text("Calling " + params.To);
+    Twilio.Device.connect(params);
+    e.preventDefault();
+});
+
+$('#incoming_accept').click(function(e) {
+
+    window.connection.accept();
+    $('#incoming_accept').hide();
     e.preventDefault();
 });
